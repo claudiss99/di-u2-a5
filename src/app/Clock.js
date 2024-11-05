f14d281c548192d4e8cf3cfdcf6a70071817ba40
Claudia <claudissperez99@gmail.com>
2024-11-05 13:45:05 +0100

    empezando

--- src/app/Clock.js
+++ src/app/Clock.js
@@ -1,5 +1,8 @@
+/**
+ * 
 export default function Clock({ time }) {
     let hours = time.getHours();
+
     if (hours >= 0 && hours <= 6) {
       document.getElementById('time').className = 'night';
     } else {
@@ -7,6 +10,17 @@ export default function Clock({ time }) {
     }
     return (
       <h1 id="time">
+        {time.toLocaleTimeString()}
+      </h1>
+    );
+  }
+ */
+  export default function Clock({ time }) {
+    const hours = time.getHours();
+    const isNight = hours >= 0 && hours <= 6; // Determina si es "noche" o "día"
+    
+    return (
+      <h1 id="time" className={isNight ? 'night' : 'day'}>
         {time.toLocaleTimeString()}
       </h1>
     );
