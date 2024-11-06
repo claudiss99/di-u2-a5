d95d152ffc3b5d340428898560303e192c22e34c
Claudia <claudissperez99@gmail.com>
2024-11-06 09:49:16 +0100

    Ejercicios hechos

--- src/app/Clock.js
+++ src/app/Clock.js
@@ -1,26 +1,15 @@
-/**
- * 
+import styles from './styles.css'
 export default function Clock({ time }) {
     let hours = time.getHours();
+    let estilo;
 
     if (hours >= 0 && hours <= 6) {
-      document.getElementById('time').className = 'night';
+      estilo = 'night'
     } else {
-      document.getElementById('time').className = 'day';
+      estilo = 'day'
     }
-    return (
-      <h1 id="time">
-        {time.toLocaleTimeString()}
-      </h1>
-    );
-  }
- */
-  export default function Clock({ time }) {
-    const hours = time.getHours();
-    const isNight = hours >= 0 && hours <= 6; // Determina si es "noche" o "día"
-    
-    return (
-      <h1 id="time" className={isNight ? 'night' : 'day'}>
+    return(
+      <h1 id="time" className={estilo}>
         {time.toLocaleTimeString()}
       </h1>
     );
